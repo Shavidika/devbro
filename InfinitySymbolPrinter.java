e475440f4d1ae237b9778374f265bf10e208ede9
Shavidika <shavidika.ekanayake@gmail.com>
2024-05-26 03:19:33 +0530

    major part

---
+++ InfinitySymbolPrinter.java
@@ -0,0 +1,14 @@
+public class InfinitySymbolPrinter {
+
+    public static void main(String[] args) {
+        String infinitySymbol = 
+            "     ____            ____\n" +
+            "  __/    \\__      __/    \\__\n" +
+            " /  \\____/  \\    /  \\____/  \\\n" +
+            "|           |  |           |\n" +
+            " \\  /\\  /\\  /    \\  /\\  /\\  /\n" +
+            "  \\/  \\/  \\/      \\/  \\/  \\/\n";
+        
+        System.out.println(infinitySymbol);
+    }
+}
